fb4aa34944dcc68bec9d659e6a32caec42281cc7
daviventura <155470809+daviventura@users.noreply.github.com>
2024-01-04 20:13:43 -0300

    Update home.py

--- home.py
+++ home.py
@@ -14,12 +14,12 @@ st.set_page_config(
 
 
 clubes=df_data['Club'].unique()
-club=st.sidebar.selectbox('Clube',clubes)
+club=st.selectbox('Clube',clubes)
 
 
 df_players=df_data[df_data['Club']==club]
 jogadores=df_players['Name'].unique()
-jogador=st.sidebar.selectbox('Jogador',jogadores)
+jogador=st.selectbox('Jogador',jogadores)
 
 
 jogador_stats=df_data[df_data['Name']==jogador].iloc[0]
@@ -30,12 +30,12 @@ st.title(jogador_stats['Name'])
 st.markdown(f"**Clube:**  {jogador_stats['Club']}")
 st.markdown(f"**Posição:**  {jogador_stats['Position']}")
 
-col1,col2,col3,col4=st.columns(4)
+# col1,col2,col3,col4=st.columns(4)
 
-col1.markdown(f"**Idade:**  {jogador_stats['Age']}")
-col2.markdown(f"**Altura:**  {jogador_stats['Height(cm.)']/100}")
-col3.markdown(f"**Peso:**  {jogador_stats['Weight(lbs.)']*0.453:.2f}")
-st.divider()
+# col1.markdown(f"**Idade:**  {jogador_stats['Age']}")
+# col2.markdown(f"**Altura:**  {jogador_stats['Height(cm.)']/100}")
+# col3.markdown(f"**Peso:**  {jogador_stats['Weight(lbs.)']*0.453:.2f}")
+# st.divider()
 
 st.subheader(f"Overall {jogador_stats['Overall']}")
 
